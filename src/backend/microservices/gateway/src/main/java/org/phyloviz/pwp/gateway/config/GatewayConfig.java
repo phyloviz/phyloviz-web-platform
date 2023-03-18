b7cac16bdd99dbf7302a176d93d3c5b7a94f9794
André Páscoa <andre@pascoa.org>
2023-03-18 12:35:54 +0000

    Added a redis memory database configuration in docker compose for storing gateway's web session data, which includes OAuth2 authorized client information (refresh token, access token, etc..)

---
+++ src/backend/microservices/gateway/src/main/java/org/phyloviz/pwp/gateway/config/GatewayConfig.java
@@ -0,0 +1,39 @@
+package org.phyloviz.pwp.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .csrf()
+                .and()
+                .oauth2Client((oauth2Client) ->
+                        // Saves the authorized client (containing access, refresh token, etc)
+                        // in the spring session. since we included the redis session dependency
+                        // the spring session will be saved in redis
+                        // so the authorized client will be saved in redis too
+                        oauth2Client
+                                .authorizedClientRepository(authorizedClientRepository())
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
+    }
+
+}
