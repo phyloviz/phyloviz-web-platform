744f03268790caadf7002fe5de5b279e581c4d6b
André Páscoa <andre@pascoa.org>
2023-04-16 17:19:03 +0100

    Updated phylodb jar.

--- src/backend/microservices/visualization/src/main/java/org/phyloviz/pwp/visualization/VisualizationMicroserviceApplication.java
+++ src/backend/microservices/visualization/src/main/java/org/phyloviz/pwp/visualization/VisualizationMicroserviceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VisualizationMicroserviceApplication {
 
+
     /**
      * Entry point for the Visualization Microservice.
      *
