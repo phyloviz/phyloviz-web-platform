9e3fababff168d5c67acc7bdfc7109a555206307
Nyck <nyckbrandao1236@gmail.com>
2024-10-21 15:23:44 +0100

    docs: update scripts README.md

--- scripts/flowviz_setup.sh
+++ scripts/flowviz_setup.sh
@@ -35,4 +35,4 @@ docker compose up -d
 echo "FLOWViZ docker setup completed!"
 
 echo "Steps 1 to 3 of the manual setup were executed successfully!"
-echo "Now you still need to execute steps 4 to 8 of the manual setup. (https://github.com/devandrepascoa/FLOWViZ?tab=readme-ov-file#manual-setup)"
+echo "Now you still need to execute steps 4 to 7 of the manual setup. (https://github.com/devandrepascoa/FLOWViZ?tab=readme-ov-file#manual-setup)"
